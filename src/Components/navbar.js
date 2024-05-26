e3538dc7fc5bd74e57b35881c3032e47b6f8205c
SenyorSchmidt <155657963+SenyorSchmidt@users.noreply.github.com>
2024-05-26 17:50:26 +0200

    Navbar implemented. Needs finishing work

---
+++ src/Components/navbar.js
@@ -0,0 +1,78 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import {
+    faFacebook,
+    faGithub,
+    faInstagram,
+    faLinkedin,
+    faMedium,
+    faStackOverflow,
+} from "@fortawesome/free-brands-svg-icons";
+import { Box, HStack } from "@chakra-ui/react";
+
+const socialMedia = [
+    {
+        icon: faEnvelope,
+        url: "mailto: robert.schmidt@bblabla.de"
+    },
+    {
+        icon: faInstagram,
+        url: "www.instagram.com"
+    },
+    {
+        icon: faFacebook,
+        url: "www.facebook.com"
+    }
+]
+
+const Navbar = () => {
+
+    const handleClick = (anchor) => () => {
+        const id = `${anchor}-section`;
+        const element = document.getElementById(id);
+        if (element) {
+            element.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+    }
+
+    return (
+        <Box
+            position="fixed"
+            top={0}
+            left={0}
+            right={0}
+            translateY={0}
+            transitionProperty="transform"
+            transitionDuration=".3s"
+            transitionTimingFunction="ease-in-out"
+            backgroundColor="#EAE7DC">
+            <Box color="black" maxWidth="1280px" margin="0 auto">
+                <HStack
+                    px={16}
+                    py={4}
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <nav>
+                        <HStack spacing={8}>
+                            <a href="/#home" onClick={handleClick("home")}>Home</a>
+                            <a href="/#aboutme" onClick={handleClick("aboutme")}>Aboute me</a>
+                            <a href="/#portfolio" onClick={handleClick("portfolio")}>Portfolio</a>
+                            <a href="/#kontakt" onClick={handleClick("kontakt")}>Kontakt</a>
+                        </HStack>
+                    </nav>
+                    <nav>
+                        <HStack spacing={8}>
+                            {socialMedia.map((social => <a href={social.url}><FontAwesomeIcon icon={social.icon} size="2x" /></a>))}
+                        </HStack>
+                    </nav>
+                </HStack>
+            </Box>
+        </Box>
+    )
+}
+
+export default Navbar
